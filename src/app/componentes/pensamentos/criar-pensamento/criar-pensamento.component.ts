2493e6ff4c98b58cdfbbf2f5a54ec7ace949c089
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-12-06 09:13:40 -0300

    Entendo o fluxo de dados
    - Property binding
    - Two-way Data Binding
    - Auto Import

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado")
+  }
+
+  cancelarPensamento() {
+    alert("Pensamento cancelado")
+  }
 
 }
