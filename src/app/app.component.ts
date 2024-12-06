786c414ae7210da0391372f84341f2f75fceb3d1
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-12-06 10:24:21 -0300

    Navegação com roteamento
    - Navegação dinâmica
    - Configuração de rotas
    - Navegação com routerLink
    - AppModule (tradicional) e Standalone Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
-import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
